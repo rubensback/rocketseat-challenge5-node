1af4e0881996dcb804344b287ca9be5dbcee94a3
Rubens Back <rubensback@gmail.com>
2020-10-10 02:35:11 +0100

    finish challenge

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: Request): Transaction {
     // TODO
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total - value < 0) {
+      throw new Error("You don't have money for this");
+    }
+
+    const transactionCreate = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transactionCreate;
   }
 }
 
